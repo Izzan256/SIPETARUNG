af8be529b9396c60f1ee27fe5c426debc8c1353d
Blues <lukmanaffandi900@gmail.com>
2026-02-24 16:13:51 +0700

    Menghapus hal-hal yang tidak penting

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 
+int microwave_sensor = NULL; // Ubah ini sesuai dengan produk jadi
+
+
+
 void setup() {
-  // put your setup code here, to run once:
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 }
 
